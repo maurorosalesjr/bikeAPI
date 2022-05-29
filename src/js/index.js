33e168cd11fa164f82a5ed77890b2dfc6b7f69b9
Mauro Rosales Jr <mauro.rosales247@gmail.com>
2022-05-29 16:27:25 -0700

    code to reformat html, adding img is WIP

--- src/js/index.js
+++ src/js/index.js
@@ -17,21 +17,29 @@ function getElements(response) {
   if(response.bikes) {
     const allBikes = response.bikes;
 
+    let bikeArray = ["Desc", "Local", "Manu", "thumb"];
 
-  
-    allBikes.forEach(function(bike) {
-      $('.showDescription').append(`This bikes description is ${bike.description}, `);
-      $('.showLocation').append(`The bike was last seen at ${bike.stolen_location}, `);
-      $('.showManufacturer').append(`The bike is manufactured by ${bike.manufacturer_name}, `);
-      $('.showErrors').append("");
-
-
-    });   
+    $('.showDescription').text(bikesorter(bikeArray, allBikes)); 
+    //console.log(bikesorter(bikearray, allBikes));
   } else {
       $('.showErrors').text(`There was an error: ${response.message}`);
   }
 }
 
+function bikesorter (bikeArray, allBikes)
+{
+  let bikeFacts = " "
+  for(var i = 0; i < allBikes.length; i++)
+  {
+    bikeArray[0] = (allBikes[i].description);
+    bikeArray[1] = (allBikes[i].stolen_location);
+    bikeArray[2] = (allBikes[i].manufacturer_name); 
+    bikeArray[3] = (allBikes[i].thumb);
+    bikeFacts += ("This bikes description is " + bikeArray[0] + " \n" + "The bike was stolen at " + bikeArray[1] + " \n" + "The bike is manufactured by " + bikeArray[2] + "\n" + `<img src='${bikeArray[3]}'>`  + " \n\n");
+
+  }
+  return bikeFacts;
+}  
   
 
 $(document).ready(function() {
